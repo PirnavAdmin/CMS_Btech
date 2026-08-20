ef4d6f885ab5ee4c034016f01b4e633f828dca39
chinnalurupravallika789-cloud <chinnalurupravallika789@gmail.com>
2026-08-20 12:30:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
 import { ROLES } from './auth/roles'
 
-import Attendance from './pages/Attendance'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import MySubjects from './pages/MySubjects'
 import Unauthorized from './pages/Unauthorized'
-import Users from './pages/Users'
 import CourseList from './pages/courseManagement/CourseList'
 import CourseFormPage from './pages/courseManagement/CourseFormPage'
 import CourseDetails from './pages/courseManagement/CourseDetails'
@@ -40,7 +38,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
 
-      {/* Users - Admin Only */}
+      {/* Administration - Admin Only */}
       <Route
         element={
           <ProtectedRoute
@@ -48,7 +46,6 @@ export default function App() {
           />
         }
       >
-      <Route path="/users" element={<Users />} />
       <Route path="/college-institution-management" element={<CollegeInstitutionManagement />} />
       <Route path="/academic-year-management" element={<AcademicYearManagement />} />
       <Route path="/department-management" element={<DepartmentManagement />} />
@@ -65,20 +62,6 @@ export default function App() {
         <Route path="/branches/:id" element={<BranchDetails />} />
         <Route path="/branches/:id/edit" element={<BranchFormPage />} />
         <Route path="/courses/:courseId/branches/:branchId/structure" element={<CourseStructure />} />
-      </Route>
-
-      {/* Attendance - Admin + Faculty */}
-      <Route
-        element={
-          <ProtectedRoute
-            allowedRoles={[
-              ROLES.ADMIN,
-              ROLES.FACULTY,
-            ]}
-          />
-        }
-      >
-        <Route path="/attendance" element={<Attendance />} />
       </Route>
 
       {/* My Subjects - Faculty + Student */}
