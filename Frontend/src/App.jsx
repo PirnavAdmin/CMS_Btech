8faedf9f04da2c543b5654be1256627bc68cb4e6
chinnalurupravallika789-cloud <chinnalurupravallika789@gmail.com>
2026-08-24 17:00:36 +0530

    update course management

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,13 +7,8 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import MySubjects from './pages/MySubjects'
 import Unauthorized from './pages/Unauthorized'
-import CourseList from './pages/courseManagement/CourseList'
-import CourseFormPage from './pages/courseManagement/CourseFormPage'
-import CourseDetails from './pages/courseManagement/CourseDetails'
-import BranchList from './pages/courseManagement/BranchList'
-import BranchFormPage from './pages/courseManagement/BranchFormPage'
-import BranchDetails from './pages/courseManagement/BranchDetails'
-import CourseStructure from './pages/courseManagement/CourseStructure'
+import Course, { CourseStructure } from './pages/courseManagement/Course'
+import Branch from './pages/courseManagement/Branch'
 import CollegeInstitutionManagement from './pages/admin-management/CollegeInstitutionManagement'
 import AddCollege from './pages/admin-management/AddCollege'
 import AcademicYearManagement from './pages/admin-management/AcademicYearManagement'
@@ -61,14 +56,14 @@ export default function App() {
 
       {/* Course & Branch Management - Admin Only */}
       <Route element={<ProtectedRoute allowedRoles={[ROLES.ADMIN]} />}>
-        <Route path="/courses" element={<CourseList />} />
-        <Route path="/courses/add" element={<CourseFormPage />} />
-        <Route path="/courses/:id" element={<CourseDetails />} />
-        <Route path="/courses/:id/edit" element={<CourseFormPage />} />
-        <Route path="/branches" element={<BranchList />} />
-        <Route path="/branches/add" element={<BranchFormPage />} />
-        <Route path="/branches/:id" element={<BranchDetails />} />
-        <Route path="/branches/:id/edit" element={<BranchFormPage />} />
+        <Route path="/courses" element={<Course />} />
+        <Route path="/courses/add" element={<Course mode="form" />} />
+        <Route path="/courses/:id" element={<Course mode="details" />} />
+        <Route path="/courses/:id/edit" element={<Course mode="form" />} />
+        <Route path="/branches" element={<Branch />} />
+        <Route path="/branches/add" element={<Branch mode="form" />} />
+        <Route path="/branches/:id" element={<Branch mode="details" />} />
+        <Route path="/branches/:id/edit" element={<Branch mode="form" />} />
         <Route path="/courses/:courseId/branches/:branchId/structure" element={<CourseStructure />} />
       </Route>
 
